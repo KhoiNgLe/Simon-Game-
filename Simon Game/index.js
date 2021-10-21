9be4f3012eea35465656e2de821e3856bc20e0b2
KhoiNgLe <91035280+KhoiNgLe@users.noreply.github.com>
2021-10-20 23:01:27 -0400

    Update Js file

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,13 +1,38 @@
-buttonColors = ["red", "blue", "green", "yellow"];
-gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern=[];
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+
 
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
+  var randomNumber = Math.floor(Math.random() * 4   );
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-  $("#randomChosenColor").css({opacity:0});
-  $("#randomChosenColor").play;
-  }
 
-  var audio = new Audio('audio_file.mp3');
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+
+}
+
+
+
+function playSound(name){
+  var audio = new Audio("C:\\Users\\16139\\OneDrive\\Desktop\\Web Development\\Simon Game\\sounds\\"+randomChosenColor+".mp3");
   audio.play();
+}
+
+function animatePress(currentColour){
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
